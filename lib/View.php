0ff2e9668d41eff81faf77ca569b022275aab21b
Michał Łoza <scorneds@gmail.com>
2012-04-06 19:19:29 +0200

    Asd

--- lib/View.php
+++ lib/View.php
@@ -12,14 +12,15 @@ class lib_View {
 	
 	public function __construct($viewname)
 	{
+		$viewname = explode('/', $viewname);
+		foreach($viewname as $i=>$v)
+		{
+			$viewname[$i] = ucfirst($v); // asd
+		}
+		$viewname = join('/', $viewname);
+		
 		if(file_exists(__DIR__.'/../views/'.$viewname.'.php')) 
 		{
-			$viewname = explode('/', $viewname);
-			foreach($viewname as $i=>$v)
-			{
-				$viewname[$i] = ucfirst($v); // asd
-			}
-			$viewname = join('/', $viewname);
 			$this->template = __DIR__.'/../views/'.$viewname.'.php';
 		}
 		else throw new Exception("View file does not exists, trying to load: ".$viewname);
