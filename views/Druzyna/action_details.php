ad5c131993eba3a161f8399ce2bf8ffdaec68a96
unknown <Scroot@Scroot-PC.(none)>
2012-04-08 16:44:44 +0200

    uptade

--- views/Druzyna/action_details.php
+++ views/Druzyna/action_details.php
@@ -56,6 +56,42 @@
 				<div class="clear"></div>
 			</div>
 		</div>
+		<br> <br>
+		<div class="block-border">
+			<div class="block-content">
+				<div class="h1">
+					<h1>Nagrody</h1>
+				</div>
+				<div>
+					<table class="table" cellspacing="0" width="100%">
+
+						<thead>
+							<tr>
+								<th class="black-cell"></th>
+								<th scope="col">Nazwa</th>
+								<th scope="col">Data</th>
+							</tr>
+						</thead>
+
+						<tbody>
+							<?php $p = $team->getNagroda(); while($n = $p->fetchObject()) { ?>
+							<tr
+								onclick="document.location.href='/nagroda/details/<?php echo $n->idDruzyny ?>.html';"
+								style="cursor: pointer;">
+								<th scope="row" class="table-check-cell"><input type="checkbox"
+									name="selected[]" id="table-selected-1" value="1"></th>
+								<td><?php echo $n->nazwaNagrody ?></td>
+								<td><?php echo $n->data ?></td>
+								
+							</tr>
+							<?php }?>
+						</tbody>
+
+					</table>
+				</div>
+				<div class="clear"></div>
+			</div>
+		</div>
 	</section>
 </article>
 <?php else : ?>
