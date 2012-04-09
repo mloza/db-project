ee3b8080826f2aa69f253cc9700ac0d89fbfba6d
anka <Scroot@Scroot-PC.(none)>
2012-04-09 23:33:37 +0200

    dodawanie

--- controller/Trener.php
+++ controller/Trener.php
@@ -39,6 +39,32 @@ class Controller_Trener extends lib_controller{
 		}
 	}
 	
+	public function action_add()
+	{
+		$this->subcurrent = 'trener-add';
+		$this->breadcrumbs['trener/add'] = 'Dodawawnie nowego trenera';
+	
+		$this->template->view->set('trener', $this->model);
+	
+		if(!empty($_POST) AND is_array($_POST)) {
+			//sprawdzamy czy podano wszstkie pola które trzeba
+			if(!empty($_POST['imie']) AND !empty($_POST['nazwisko']) AND !empty($_POST['odKiedy']) AND !empty($_POST['dataUr']))
+			{
+				if(!($e = $this->model->add($_POST)))
+					$this->redirect('/trener/list/added.html');
+				else {
+					$this->template->set('msg', array('type' => "error", 'msg' => "Błąd bazy danych: ".$e));
+				}
+			} else $this->template->set('msg', array('type' => "error", 'msg' => "Nie uzupełniłeś wymaganych pól"));
+		}
+	}
+	
+	public function action_delete($id)
+	{
+		$this->model->delete($id);
+		$this->redirect('/trener/list/deleted.html');
+	}
+	
 	public function action_details($id = NULL)
 	{
 		$trener = $this->model->getTrener($id);
